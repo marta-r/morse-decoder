212fbc98c94a53e1e97d5c2408cfec2a476e0603
Marta Rudkouskaya <lvlartik@gmail.com>
2020-03-18 16:39:40 +0300

    feat: add solution for Morse code decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr
+        .replace(/\S{10}/gi, "$& ")
+        .trim()
+        .split(" ");
+    let arrDecode = [];
+    let arrReadable = [];
+
+    arrDecode = arr.map(function(item) {
+        let arrtemp = item
+            .replace(/\S{2}/gi, "$& ")
+            .trim()
+            .split(" ");
+        let pointsDashes = [];
+        for (let a = 4; a > -1; a--) {
+            if (arrtemp[a] == "**") {
+                pointsDashes.push(" ");
+                break;
+            } else if (arrtemp[a] == "10") {
+                pointsDashes.push(".");
+            } else if (arrtemp[a] == "11") {
+                pointsDashes.push("-");
+            } else if (arrtemp[a] == "00") break;
+        }
+        return pointsDashes.reverse().join("");
+    });
+
+    for (let b = 0; b < arrDecode.length; b++) {
+        if (arrDecode[b] == " ") {
+            arrReadable.push(" ");
+        } else arrReadable.push(MORSE_TABLE[arrDecode[b]]);
+    }
+
+    return arrReadable.join("");
 }
 
 module.exports = {
